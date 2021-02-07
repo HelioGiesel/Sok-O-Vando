73d5a9ab8b2b82b2bb82032a36d53d573744ca16
Helio Giesel <heliogiesel@gmail.com>
2021-02-07 15:59:20 +0000

    Added box and checkEdges class

---
+++ Sok-O-Vando/src/org/academiadecodigo/asynctomatics/sokovando/Box.java
@@ -0,0 +1,17 @@
+package org.academiadecodigo.asynctomatics.sokovando;
+
+public class Box{
+    private Position pos;
+
+    public Box(Position pos){
+        this.pos = pos;
+    }
+
+    public boolean receiveMove(Direction direction){
+        if (!checkEdges.box(pos.move(direction)) && (!checkEdges.wall(pos.move(direction)))) {
+            pos.move(direction);
+            return true;
+        }
+        return false;
+    }
+}
