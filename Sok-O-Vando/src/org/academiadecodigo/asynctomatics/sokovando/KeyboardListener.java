627bb55cc511f93857cd112010107ae9d6ceb1c3
VascoFerraz <vascoferraz@gmail.com>
2021-02-07 14:54:55 +0000

    Add skeleton of the KeyboardListener

---
+++ Sok-O-Vando/src/org/academiadecodigo/asynctomatics/sokovando/KeyboardListener.java
@@ -0,0 +1,84 @@
+package org.academiadecodigo.asynctomatics.sokovando;
+
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
+public class KeyboardListener implements KeyboardHandler {
+
+    public void setup() {
+        Keyboard keyboard = new Keyboard(this);
+
+        // Event of pressing the UP arrow
+        KeyboardEvent upArrowPressed = new KeyboardEvent();
+        upArrowPressed.setKey(KeyboardEvent.KEY_UP);
+        upArrowPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        keyboard.addEventListener(upArrowPressed);
+
+        // Event of releasing the UP arrow
+        KeyboardEvent upArrowR = new KeyboardEvent();
+        upArrowR.setKey(KeyboardEvent.KEY_UP);
+        upArrowR.setKeyboardEventType(KeyboardEventType.KEY_RELEASED);
+        keyboard.addEventListener(upArrowR);
+
+        // Event of pressing the DOWN arrow
+        KeyboardEvent downArrowPressed = new KeyboardEvent();
+        downArrowPressed.setKey(KeyboardEvent.KEY_DOWN);
+        downArrowPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        keyboard.addEventListener(downArrowPressed);
+
+        // Event of releasing the DOWN arrow
+        KeyboardEvent downArrowReleased = new KeyboardEvent();
+        downArrowReleased.setKey(KeyboardEvent.KEY_DOWN);
+        downArrowReleased.setKeyboardEventType(KeyboardEventType.KEY_RELEASED);
+        keyboard.addEventListener(downArrowReleased);
+
+        // Event of pressing the LEFT arrow
+        KeyboardEvent leftArrowPressed = new KeyboardEvent();
+        leftArrowPressed.setKey(KeyboardEvent.KEY_LEFT);
+        leftArrowPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        keyboard.addEventListener(leftArrowPressed);
+
+        // Event of releasing the LEFT arrow
+        KeyboardEvent leftArrowReleased = new KeyboardEvent();
+        leftArrowReleased.setKey(KeyboardEvent.KEY_LEFT);
+        leftArrowReleased.setKeyboardEventType(KeyboardEventType.KEY_RELEASED);
+        keyboard.addEventListener(leftArrowReleased);
+
+        // Event of pressing the RIGHT arrow
+        KeyboardEvent rightArrowPressed = new KeyboardEvent();
+        rightArrowPressed.setKey(KeyboardEvent.KEY_RIGHT);
+        rightArrowPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        keyboard.addEventListener(rightArrowPressed);
+
+        // Event of releasing the RIGHT arrow
+        KeyboardEvent rightArrowReleased = new KeyboardEvent();
+        rightArrowReleased.setKey(KeyboardEvent.KEY_RIGHT);
+        rightArrowReleased.setKeyboardEventType(KeyboardEventType.KEY_RELEASED);
+        keyboard.addEventListener(rightArrowReleased);
+
+    }
+
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+        if (keyboardEvent.getKey() == 38) { System.out.println("UP KEY    : Pressed"); }
+        if (keyboardEvent.getKey() == 40) { System.out.println("DOWN KEY  : Pressed"); }
+        if (keyboardEvent.getKey() == 39) { System.out.println("RIGHT KEY : Pressed"); }
+        if (keyboardEvent.getKey() == 37) { System.out.println("LEFT KEY  : Pressed"); }
+
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+        if (keyboardEvent.getKey() == 38) { System.out.println("UP KEY    : Released"); }
+        if (keyboardEvent.getKey() == 40) { System.out.println("DOWN KEY  : Released"); }
+        if (keyboardEvent.getKey() == 39) { System.out.println("RIGHT KEY : Released"); }
+        if (keyboardEvent.getKey() == 37) { System.out.println("LEFT KEY  : Released"); }
+
+    }
+}
+
+
+
--- Sok-O-Vando/src/org/academiadecodigo/asynctomatics/sokovando/Main.java
+++ Sok-O-Vando/src/org/academiadecodigo/asynctomatics/sokovando/Main.java
@@ -5,8 +5,13 @@ import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
+
+        KeyboardListener keyboard = new KeyboardListener();
+        keyboard.setup();
+
         Rectangle test = new Rectangle(20,20,150,150);
         test.setColor(Color.RED);
         test.fill();
+
     }
 }
