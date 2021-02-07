02783b012ed7919b0ba7d56295cb15ff1baa6de3
VascoFerraz <vascoferraz@gmail.com>
2021-02-07 21:39:35 +0000

    Add player's receiveMove method

--- Sok-O-Vando/src/org/academiadecodigo/asynctomatics/sokovando/Player.java
+++ Sok-O-Vando/src/org/academiadecodigo/asynctomatics/sokovando/Player.java
@@ -5,26 +5,32 @@ import java.util.Scanner;
 public class Player {
 
     private String name;
+    private Position pos;
 
-    public boolean checkEdges() {
-        return true;
+    public Player(Position pos) {
+        this.pos = pos;
     }
 
-    public void move() {}
+    public boolean receiveMove(Direction direction) {
+        if (!checkEdges.player(pos.move(direction)) && (!checkEdges.wall(pos.move(direction))) && (!checkEdges.box(pos.move(direction)))) {
+            pos.move(direction);
+            return true;
+        }
+        return false;
+    }
 
-    public boolean isSpot() {
-        return true;
+    public void setName() {
+        System.out.print("Choose a name: ");
+        Scanner scanner = new Scanner(System. in);
+        this.name = scanner. nextLine();
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName() {
-        
-        System.out.print("Choose a name: ");
-        Scanner scanner = new Scanner(System. in);
-        this.name = scanner. nextLine();
-    }
 
 }
+
+
+
